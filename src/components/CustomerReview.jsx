7778de15c6ecd2a01fb58e25569f272eea775fa6
mohammedelwardany <mohammed.aael@fci.suezuni.edu.eg>
2024-11-25 01:40:36 +0200

    why us responsive handeled

--- src/components/CustomerReview.jsx
+++ src/components/CustomerReview.jsx
@@ -3,15 +3,15 @@ import StarRating from "./StarRate";
 import UserTag from "./UserTag";
 import TruncateText from "@/utilites/TextTrancate";
 
-const CustomerReview = ({Review,MainColor="white"}) => (
-    <Stack direction="column" className='flex flex-col items-center justify-center'>
-            <Stack spacing={2} direction="row" className='flex flex-row items-center justify-between w-full px-3 mb-2'>
-            <UserTag userImage={Review.userImage} userName={TruncateText(Review.userName , 16)} size={30}/>
+const CustomerReview = ({ Review, MainColor = "white" }) => (
+  <Stack direction="column" className='flex flex-col items-center justify-center'>
+    <Stack spacing={2} direction="row" className='flex flex-row items-center justify-between w-full px-3 mb-2'>
+      <UserTag userImage={Review.userImage} userName={TruncateText(Review.userName, 16)} size={30} />
 
-      <StarRating rating={Review.rating}/>
-</Stack>
+      <StarRating rating={Review.rating} />
+    </Stack>
 
-    <Box 
+    <Box
       sx={{
         width: "100%",
         height: 200,
@@ -21,13 +21,13 @@ const CustomerReview = ({Review,MainColor="white"}) => (
         alignItems: 'center',
         justifyContent: 'center',
         border: `2px solid ${MainColor}`,
-        paddingX:4,
-        paddingY:4,
-        marginX:5
+        paddingX: 4,
+        paddingY: 4,
+        marginX: 5
       }}
     >
-    <h3 style={{color:MainColor}} className="md:text-md text-sm">{Review.text}</h3>
+      <h3 style={{ color: MainColor }} className="md:text-md text-sm">{Review.text}</h3>
     </Box>
-    </Stack>
-  );
+  </Stack>
+);
 export default CustomerReview;
